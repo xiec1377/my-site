3d999ba9fcd517f2335341be8cad64e0823ae558
Catherine Xie <catherinexie2014@gmail.com>
2023-02-03 02:26:53 -0500

    Initialize project using Create React App Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import CloseBar from './components/CloseBar/CloseBar';
-import NavBar from './components/NavBar/NavBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg'
+import './App.css'
+import CloseBar from './components/CloseBar/CloseBar'
+import NavBar from './components/NavBar/NavBar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home/Home';
 
 function App() {
   return (
     <>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" exact />
-      </Routes>
-    </Router>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+        </Routes>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
