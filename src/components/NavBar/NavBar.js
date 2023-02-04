21751021276596f6d9fb214cda4f8b5a68ce1989
Catherine Xie <catherinexie2014@gmail.com>
2023-02-04 02:15:06 -0500

    Create pages menu nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,35 +3,44 @@ import { Link } from 'react-router-dom'
 import { Button } from '../Button/Button'
 import './NavBar.css'
 
+// {
+//   button && <Button buttonStyle="btn--dark">home</Button>
+// }
+
 function NavBar() {
   const [button] = useState(true)
   return (
     <>
+      <div className="title">pages</div>
       <nav className="navbar">
         <div className="navbar-container">
           <ul>
-            {button && (
-              <Button buttonStyle="btn--dark">
-                home
-              </Button>
-            )}
-
             <li className="nav-item">
+              <div className="nav-icon"></div>
+              <Link to="/home" className="nav-links">
+                home
+              </Link>
+            </li>
+            <li className="nav-item">
+            <div className="nav-icon"></div>
               <Link to="/about-me" className="nav-links">
                 about me
               </Link>
             </li>
             <li className="nav-item">
+            <div className="nav-icon"></div>
               <Link to="/experiences" className="nav-links">
                 experiences
               </Link>
             </li>
             <li className="nav-item">
+            <div className="nav-icon"></div>
               <Link to="/projects" className="nav-links">
                 projects
               </Link>
             </li>
             <li className="nav-item">
+            <div className="nav-icon"></div>
               <Link to="/hobbies" className="nav-links">
                 hobbies
               </Link>
--- src/components/StaticInterface/StaticInterface.js
+++ src/components/StaticInterface/StaticInterface.js
@@ -12,6 +12,7 @@ export default function StaticInterface() {
       <TopStaticBar />
       <SideStaticBar />
       <ColourPicker />
+      <NavBar />
     </>
   )
 }
