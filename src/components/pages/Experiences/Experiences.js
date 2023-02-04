1840c90821426588cd5d14b51833f3d03beda493
Catherine Xie <catherinexie2014@gmail.com>
2023-02-04 04:43:16 -0500

    Create experiences page component

---
+++ src/components/pages/Experiences/Experiences.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Footer from '../../Footer/Footer'
+import StaticInterface from '../../StaticInterface/StaticInterface'
+import '../../staticCSS.css'
+import './Experiences.css'
+
+export default function Experiences() {
+    return (
+        <>
+          <StaticInterface />
+          <div className="page">
+            <div className="page-title">
+              <h1>experiences.</h1>
+            </div>
+          </div>
+          <Footer />
+        </>
+      )
+    }
+    
