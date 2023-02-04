8dead6e8c5eb713f7d25c4073b7be58fcf7052b8
Catherine Xie <catherinexie2014@gmail.com>
2023-02-04 04:53:31 -0500

    Create hobbies page component

---
+++ src/components/pages/Hobbies/Hobbies.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../../Footer/Footer'
+import StaticInterface from '../../StaticInterface/StaticInterface'
+import '../../staticCSS.css'
+import './Hobbies.css'
+
+export default function Hobbies() {
+  return (
+    <>
+      <StaticInterface />
+      <div className="page">
+        <div className="page-title">
+          <h1>hobbies.</h1>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
