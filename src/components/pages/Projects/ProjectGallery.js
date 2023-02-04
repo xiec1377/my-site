34e628b0c7f0f7384cbb357626cda5edb78ec93e
Catherine Xie <catherinexie2014@gmail.com>
2023-02-04 16:10:15 -0500

    Updated projects gallery

---
+++ src/components/pages/Projects/ProjectGallery.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './ProjectGallery.css'
+
+function ProjectGallery() {
+  return (
+    <div className="gallery-container">
+      <a href="https://devpost.com/software/north-arcadia" className="img-link">
+        <img
+          src={require('./sales_pitch_generator.jpg')}
+          alt="sales-pitch-generator"
+          className="gallery-item-1"
+        />
+      </a>
+      <a href="https://devpost.com/software/search-with-tech" className="img-link">
+        <img
+          src={require('./search_with_tech.jpg')}
+          alt="search-with-tech"
+          className="gallery-item-2"
+        />
+      </a>
+
+      <a href="https://devpost.com/software/pitcheasy" className="img-link">
+        <img
+          src={require('./north_arcadia.jpg')}
+          alt="north-arcadia"
+          className="gallery-item-3"
+        />
+      </a>
+    </div>
+  )
+}
+
+export default ProjectGallery
