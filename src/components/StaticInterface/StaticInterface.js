765bea7e683ef47a16af7ef91a7a02a411af317e
Catherine Xie <catherinexie2014@gmail.com>
2023-02-05 16:19:44 -0500

    Update

--- src/components/StaticInterface/StaticInterface.js
+++ src/components/StaticInterface/StaticInterface.js
@@ -24,7 +24,7 @@ export default function StaticInterface() {
       <ColourPicker />
       {/* <SketchPicker />
       <ChromePicker /> */}
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
